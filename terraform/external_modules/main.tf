ef0c507c22bfaf9d1b3ff3e16a54278919c3f08c
bc-owner-auto <110401622+bc-owner-auto@users.noreply.github.com>
2023-02-12 15:24:13 +0200

    Delete terraform/external_modules directory

--- terraform/external_modules/main.tf
+++
@@ -1,5 +0,0 @@
-module "terragoat_module" {
-  source = "git::https://github.com/bridgecrewio/terraform-azurerm-bridgecrew-azure-read-only.git"
-  bridgecrew_token = "test_token"
-  org_name = "tests"
-}
